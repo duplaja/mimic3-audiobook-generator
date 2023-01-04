cf1c80697a222faddabdec251a538cf9a0645fe5
Dan D <dan.dulaney07@gmail.com>
2023-01-03 19:41:23 -0600

    Add meta tags and folder structure creation for ABS

--- audiobook-mimic.py
+++ audiobook-mimic.py
@@ -4,8 +4,21 @@ import os
 
 #Enter / change below as needed
 inputfilename = "to2c.txt"
-outputname = "tale-of-two-cities" #no spaces, no extension
+author = "Charles Dickens"
+title = "Tale of Two Cities"
+
 mimicvoice = "en_US/m-ailabs_low"
+
+#Stop Changing
+outputname = author+' - '+title
+
+outputpath = author+"/"+title
+outputname = author+" - "+title #no spaces, no extension
+
+#Makes the appropriate folder structure
+cwd = os.getcwd()
+os.mkdir(cwd+'/'+author)
+os.mkdir(cwd+'/'+outputpath)
 
 #Begins the process
 fin = open(inputfilename, "rt", encoding='utf-8')
@@ -30,13 +43,21 @@ fout.write('</speak>')
 fin.close()
 fout.close()
 
-tts_command = 'mimic3 --voice '+mimicvoice+' --ssml --length-scale 1.3 --noise-scale 0.667 < '+outputname+'.txt > temp-'+outputname+'.wav'
+tts_command = 'mimic3 --voice '+mimicvoice+' --ssml --length-scale 1.3 --noise-scale 0.667 < "'+outputname+'.txt" > "temp-'+outputname+'.wav"'
 print ('Using Mimic 3 to Convert to Audio (.wav): ' + tts_command)
 subprocess.call(tts_command, shell=True)
 
-convert_command = 'ffmpeg -i temp-'+outputname+'.wav -acodec libmp3lame '+outputname+'.mp3'
-print ('Converting to mp3: ' + convert_command)
+#MP3 Output
+#convert_command = 'ffmpeg -i "temp-'+outputname+'.wav" -metadata album="'+title+'" -metadata title="'+title+'" -metadata artist="'+author+'" -metadata album_artist="'+author+'" -acodec libmp3lame "'+outputname+'.mp3"'
+
+#M4B Output
+convert_command = 'ffmpeg -i "temp-'+outputname+'.wav" -metadata album="'+title+'" -metadata title="'+title+'" -metadata artist="'+author+'" -metadata album_artist="'+author+'" -acodec aac -ac 2 -b:a 64k -f mp4 "'+outputpath+'/'+outputname+'.m4b"'
+
+
+print ('Converting filetype and tagging: ' + convert_command)
 subprocess.call(convert_command, shell=True)
+
+#-map_metadata 0:s:a:0
 
 if os.path.exists('temp-'+outputname+'.wav'):
   os.remove('temp-'+outputname+'.wav')
